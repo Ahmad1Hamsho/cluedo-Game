c6afa7f77d1f522229284411ab80dc9439e0cc94
Ahmad1Hmasho <a1994h.sy@gmail.com>
2020-05-25 01:48:13 +0200

    cluedo

---
+++ assets/js/hard.js
@@ -0,0 +1,213 @@
+var audioX = document.getElementById("audioX");
+var audioR = document.getElementById("audioR");
+const suspectsArray = [
+  {
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Entrepreneur",
+  },
+  {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Scientist",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Designer",
+  },
+  {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    occupation: " Actor",
+  },
+  {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    occupation: "Socialité",
+  },
+  {
+    firstName: "Jack",
+    lastName: "Mustard",
+    occupation: "Retired Football player",
+  },
+];
+
+const weaponsArray = [
+  {
+    name: "rope",
+    weight: 10,
+  },
+  {
+    name: "knife",
+    weight: 8,
+  },
+  {
+    name: "candlestick",
+    weight: 2,
+  },
+  {
+    name: "dumbbell",
+    weight: 30,
+  },
+  {
+    name: "poison",
+    weight: 2,
+  },
+  {
+    name: "axe",
+    weight: 15,
+  },
+  {
+    name: "bat",
+    weight: 13,
+  },
+  {
+    name: "trophy",
+    weight: 25,
+  },
+  {
+    name: "pistol ",
+    weight: 20,
+  },
+];
+
+const roomsArray = [
+  {
+    name: "Dining Room",
+  },
+  {
+    name: "Conservatory",
+  },
+  {
+    name: "Kitchen",
+  },
+  {
+    name: "Study",
+  },
+  {
+    name: "Library",
+  },
+  {
+    name: "Billiard Room",
+  },
+  {
+    name: "Lounge",
+  },
+  {
+    name: "Ballroom",
+  },
+  {
+    name: "Hall",
+  },
+  {
+    name: "Spa",
+  },
+  {
+    name: "Living Room",
+  },
+  {
+    name: "Observatory",
+  },
+  {
+    name: "Theater",
+  },
+  {
+    name: "Guest House",
+  },
+  {
+    name: "Patio",
+  },
+];
+const rS = suspectsArray[Math.floor(Math.random() * suspectsArray.length)];
+const rW = weaponsArray[Math.floor(Math.random() * weaponsArray.length)];
+const rR = roomsArray[Math.floor(Math.random() * roomsArray.length)];
+const suspectsS = document.getElementById("suspects");
+const weaponsS = document.getElementById("weapons");
+const roomsS = document.getElementById("rooms");
+let num = 0;
+function answer() {
+  num = parseInt(num) + parseInt(1);
+  if (
+    suspectsS[suspectsS.selectedIndex].value == rS.firstName &&
+    weaponsS[weaponsS.selectedIndex].value == rW.name &&
+    roomsS[roomsS.selectedIndex].value == rR.name
+  ) {
+    audioR.play();
+    suspectsS.disabled = true;
+    weaponsS.disabled = true;
+    roomsS.disabled = true;
+    erg.innerHTML = " ";
+    erg.innerHTML = `Yeah that's right ${rS.firstName} ${rS.lastName} killed Mr. Boddy using the ${rW.name} ${rW.weight} in the ${rR.name} 🧛‍♀️`;
+  } else if (
+    suspectsS[suspectsS.selectedIndex].value !== rS.firstName &&
+    weaponsS[weaponsS.selectedIndex].value == rW.name &&
+    roomsS[roomsS.selectedIndex].value == rR.name
+  ) {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = "Noo, the Suspects name is wrong";
+  } else if (
+    (weaponsS[weaponsS.selectedIndex].value !== rW.name &&
+      suspectsS[suspectsS.selectedIndex].value) == rS.firstName &&
+    roomsS[roomsS.selectedIndex].value == rR.name
+  ) {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = "Noo, the Weapons is wrong";
+  } else if (
+    suspectsS[suspectsS.selectedIndex].value == rS.firstName &&
+    weaponsS[weaponsS.selectedIndex].value == rW.name &&
+    roomsS[roomsS.selectedIndex].value !== rR.name
+  ) {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = "Noo, the Room name is wrong";
+  } else if (
+    suspectsS[suspectsS.selectedIndex].value !== rS.firstName &&
+    weaponsS[weaponsS.selectedIndex].value !== rW.name &&
+    roomsS[roomsS.selectedIndex].value == rR.name
+  ) {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = "Noo, the Weapons and Suspects name is wrong";
+  } else if (
+    suspectsS[suspectsS.selectedIndex].value == rS.firstName &&
+    weaponsS[weaponsS.selectedIndex].value !== rW.name &&
+    roomsS[roomsS.selectedIndex].value !== rR.name
+  ) {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = "Noo, the Weapons and Room name is wrong";
+  } else if (
+    suspectsS[suspectsS.selectedIndex].value !== rS.firstName &&
+    weaponsS[weaponsS.selectedIndex].value == rW.name &&
+    roomsS[roomsS.selectedIndex].value !== rR.name
+  ) {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = " Noo, the Room name and Suspects name are wrong";
+  } else {
+    audioX.play();
+    erg.innerHTML = " ";
+    erg.innerHTML = "No, everything You've chosen is wrong";
+  }
+
+  if (num >= 4) {
+    suspectsS.style.display = "none";
+    weaponsS.style.display = "none";
+    roomsS.style.display = "none";
+    document.getElementById("body1").style.height = "40vh";
+    erg.innerHTML = " ";
+    erg.innerHTML = `Oops , You've run out of trying, and you can't find the right answer,but Detective Ahmad🕵️‍♂️ found it and is ${rS.firstName} ${rS.lastName} killed Mr. Boddy using the ${rW.name} ${rW.weight} in the ${rR.name}`;
+  }
+}
+if (suspectsS[suspectsS.selectedIndex].value == "Jacob") {
+}
+function ref() {
+  window.location.reload();
+}
+
+console.log(rS.firstName);
+console.log(rW.name);
+console.log(rR.name);
---
+++ assets/js/main.js
@@ -0,0 +1,182 @@
+const userName01 = document.getElementById("hy");
+const userName1 = localStorage.getItem("textvalue");
+userName01.innerHTML += " " + localStorage.getItem("textvalue");
+const userName2 = document.getElementById("hy1");
+userName2.innerHTML += " " + localStorage.getItem("textvalue");
+var audioX = document.getElementById("audioX");
+var audioR = document.getElementById("audioR");
+const suspectsArray = [
+  {
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Entrepreneur",
+  },
+  {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Scientist",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Designer",
+  },
+  {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    occupation: " Actor",
+  },
+  {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    occupation: "Socialité",
+  },
+  {
+    firstName: "Jack",
+    lastName: "Mustard",
+    occupation: "Retired Football player",
+  },
+];
+
+const weaponsArray = [
+  {
+    name: "rope",
+    weight: 10,
+  },
+  {
+    name: "knife",
+    weight: 8,
+  },
+  {
+    name: "candlestick",
+    weight: 2,
+  },
+  {
+    name: "dumbbell",
+    weight: 30,
+  },
+  {
+    name: "poison",
+    weight: 2,
+  },
+  {
+    name: "axe",
+    weight: 15,
+  },
+  {
+    name: "bat",
+    weight: 13,
+  },
+  {
+    name: "trophy",
+    weight: 25,
+  },
+  {
+    name: "pistol ",
+    weight: 20,
+  },
+];
+
+const roomsArray = [
+  {
+    name: "Dining Room",
+  },
+  {
+    name: "Conservatory",
+  },
+  {
+    name: "Kitchen",
+  },
+  {
+    name: "Study",
+  },
+  {
+    name: "Library",
+  },
+  {
+    name: "Billiard Room",
+  },
+  {
+    name: "Lounge",
+  },
+  {
+    name: "Ballroom",
+  },
+  {
+    name: "Hall",
+  },
+  {
+    name: "Spa",
+  },
+  {
+    name: "Living Room",
+  },
+  {
+    name: "Observatory",
+  },
+  {
+    name: "Theater",
+  },
+  {
+    name: "Guest House",
+  },
+  {
+    name: "Patio",
+  },
+];
+const erg = document.getElementById("erg");
+const cards = document.getElementById("cards");
+const refr = document.getElementById("ref");
+const card = document.querySelectorAll("#card");
+let num = 0;
+for (const cardItem of card) {
+  var rightCard = Math.round(Math.random() * card.length - 1);
+  for (var i = 0; i < card.length; i++) {
+    const rS = suspectsArray[Math.floor(Math.random() * suspectsArray.length)];
+    const rW = weaponsArray[Math.floor(Math.random() * weaponsArray.length)];
+    const rR = roomsArray[Math.floor(Math.random() * roomsArray.length)];
+    console.log(rS.firstName);
+    console.log(rS.lastName);
+    console.log(rW.name);
+    console.log(rW.weight);
+    console.log(rR.name);
+    cardItem.innerHTML = `Do you think <h3>${rS.firstName} ${rS.lastName}</h3> killed Mr. Boddy in the ${rR.name} with a ${rW.name} ${rW.weight} ?`;
+    card[i].addEventListener("click", function () {
+      num = parseInt(num) + parseInt(1);
+      console.log("num");
+      if (this === card[rightCard]) {
+        audioR.play();
+        userName01.style.display = "none";
+        userName2.style.display = "none";
+        cards.style.display = "none";
+        erg.style.position = "absolute";
+        erg.style.top = "40%";
+        erg.style.left = "4%";
+        refr.style.display = "block";
+        erg.innerHTML = `Yeah ${userName1} that's right ${rS.firstName} ${rS.lastName} killed Mr. Boddy using the ${rW.name} ${rW.weight} in the ${rR.name} 🧛‍♀️`;
+      } else if (this !== card[rightCard] && num <= 15) {
+        audioX.play();
+        erg.innerHTML = `No😨, this is wrong. In order to avoid accusing innocent people, we asked Detective Ahmad🕵️‍♂️to help us and was able to discover the weapon with which the crime took place. the weapon is (${rW.name})`;
+      } else if (this !== card[rightCard] && num >= 30) {
+        audioX.play();
+        erg.innerHTML = `Nooo🤦‍♂️, this is a mistake. We asked Detective Ahmad🕵️‍♂️ to help us again and we were able to discover the name of the criminal but we need to know the room where the crime occurred. the name is( ${rS.firstName} ) `;
+      }
+      if (this !== card[rightCard] && num >= 45) {
+        audioX.play();
+        userName01.style.display = "none";
+        userName2.style.display = "none";
+        erg.style.position = "absolute";
+        erg.style.top = "40%";
+        erg.style.left = "4%";
+        erg.innerHTML = `Oops ${userName1} , You've run out of trying, and you can't find the right answer,but Detective Ahmad🕵️‍♂️ found it and is ${rS.firstName} ${rS.lastName} killed Mr. Boddy using the ${rW.name} ${rW.weight} in the ${rR.name}`;
+        refr.style.display = "block";
+        cards.style.display = "none";
+      }
+    });
+  }
+}
+
+function ref() {
+  window.location.reload();
+}
+console.log(rightCard);
